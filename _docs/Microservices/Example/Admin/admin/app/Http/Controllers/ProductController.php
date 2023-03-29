2381f3650717357aa35eeeebe5f03b67d3c94845
Nasrullayev Alisher <myemail9445@inbox.ru>
2023-03-29 09:32:46 +0500

    microservice example admin part on docker

--- _docs/Microservices/Example/Admin/admin/app/Http/Controllers/ProductController.php
+++ _docs/Microservices/Example/Admin/admin/app/Http/Controllers/ProductController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
@@ -14,5 +16,30 @@ class ProductController extends Controller
     public function show(int $id): Product
     {
         return Product::query()->find($id);
+    }
+
+    public function store(Request $request)
+    {
+        $product = Product::query()->create($request->only('title', 'image'));
+
+        return response($product, Response::HTTP_CREATED);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::query()->find($id);
+
+        abort_if(!$product, Response::HTTP_NOT_FOUND, 'Product not found');
+
+        $product->update($request->only('title', 'image'));
+
+        return response($product, Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy($id)
+    {
+        Product::destroy($id);
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
