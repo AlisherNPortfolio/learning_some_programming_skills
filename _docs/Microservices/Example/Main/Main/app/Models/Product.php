ae2e5beaad4b36871441ed8748d9496aa3a41f3f
Nasrullayev Alisher <myemail9445@inbox.ru>
2023-03-29 13:37:59 +0500

    microservices example working with rabbitmq

--- _docs/Microservices/Example/Main/Main/app/Models/Product.php
+++ _docs/Microservices/Example/Main/Main/app/Models/Product.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    public $incrementing = false;
+
+    protected $fillable = ['id', 'title', 'image', 'created_at', 'updated_at'];
 }
--- _docs/Microservices/Example/Main/Main/app/Providers/EventServiceProvider.php
+++ _docs/Microservices/Example/Main/Main/app/Providers/EventServiceProvider.php
@@ -2,37 +2,23 @@
 
 namespace App\Providers;
 
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use App\Jobs\ProductCreated;
+use App\Jobs\ProductDeleted;
+use App\Jobs\ProductUpdated;
+// use App\Jobs\TestJob;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\App;
 
 class EventServiceProvider extends ServiceProvider
 {
-    /**
-     * The event to listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
-
     /**
      * Register any events for your application.
      */
     public function boot(): void
     {
-        //
-    }
-
-    /**
-     * Determine if events and listeners should be automatically discovered.
-     */
-    public function shouldDiscoverEvents(): bool
-    {
-        return false;
+        // App::bindMethod(TestJob::class.'@handle', fn ($job) => $job->handle());
+        App::bindMethod(ProductCreated::class.'@handle', fn ($job) => $job->handle());
+        App::bindMethod(ProductUpdated::class.'@handle', fn ($job) => $job->handle());
+        App::bindMethod(ProductDeleted::class.'@handle', fn ($job) => $job->handle());
     }
 }
