8a0264b4e788636109a04f15b2dd62a636369169
Nasrullayev Alisher <myemail9445@inbox.ru>
2023-05-01 14:56:10 +0500

    bubble sort

---
+++ _docs/AlgorithmsAndDataStructures/AlgorithmsOneByOne/SortingAlgorithms/1.bublesort.php
@@ -0,0 +1,12 @@
+<?php
+
+function bubbleSort($data)
+{
+}
+
+$data = [1, 4, 6, -7, 234, 0, -4, -6, 44, 65, 12, -98, -84, 78]; // 14
+
+$sorted = bubbleSort($data);
+echo '<pre>';
+// print_r($sorted);
+echo '</pre>';
